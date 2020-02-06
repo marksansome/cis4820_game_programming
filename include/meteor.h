33c36181a11fdefc96ec452f666fadcc354a2378
Mark Sansome <marksansome11@gmail.com>
2020-02-06 12:02:41 -0500

    adding starting code for meteor, also projectiles are working

---
+++ include/meteor.h
@@ -0,0 +1,9 @@
+/* CIS4820 Game Programming - Winter 2020
+ * Mark Sansome
+ * 0916066
+*/
+
+#ifndef msansome_meteor_h
+#define msansome_meteor_h
+
+#endif //msansome_meteor_h
---
+++ src/meteor.c
@@ -0,0 +1,13 @@
+/* CIS4820 Game Programming - Winter 2020
+ * Mark Sansome
+ * 0916066
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "meteor.h"
+
+#include "config.h"
+#include "graphics.h"
